717e51e82708ecf46fb36e2c66d2a6965b576505
Marcin Kulik <m@ku1ik.com>
2013-10-04 18:44:13 +0200

    Update aur and homebrew packages to the latest version

--- homebrew/asciinema.rb
+++ homebrew/asciinema.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Asciinema < Formula
   homepage 'http://asciinema.org'
-  url 'https://github.com/sickill/asciinema/archive/v0.9.4.tar.gz'
-  sha1 'bf9292a133ca6bf37238110762bee60e99882c85'
+  url 'https://github.com/sickill/asciinema/archive/v0.9.5.tar.gz'
+  sha1 '902893a524eaab7794eb04b5092b4b78c3c4c622'
 
   depends_on :python => "2.7"
 
