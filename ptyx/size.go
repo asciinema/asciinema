41c8b0d198c8a5a359f7a83d6357b4c63b402b15
Marcin Kulik <m@ku1ik.com>
2014-08-02 22:25:54 +0200

    Add extension to github.com/kr/pty

---
+++ ptyx/size.go
@@ -0,0 +1,31 @@
+package ptyx
+
+// Extension to github.com/kr/pty adding Setsize()
+
+import (
+	"os"
+	"syscall"
+	"unsafe"
+)
+
+type winsize struct {
+	ws_row    uint16
+	ws_col    uint16
+	ws_xpixel uint16
+	ws_ypixel uint16
+}
+
+func ioctl(fd, cmd, ptr uintptr) error {
+	_, _, e := syscall.Syscall(syscall.SYS_IOCTL, fd, cmd, ptr)
+	if e != 0 {
+		return e
+	}
+	return nil
+}
+
+func Setsize(f *os.File, rows int, cols int) error {
+	var ws winsize
+	ws.ws_row = uint16(rows)
+	ws.ws_col = uint16(cols)
+	return ioctl(f.Fd(), syscall.TIOCSWINSZ, uintptr(unsafe.Pointer(&ws)))
+}
