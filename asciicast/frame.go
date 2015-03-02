0e0f33383c0c05ec5d78f9abec8de3a2e85c98b7
Marcin Kulik <m@ku1ik.com>
2015-03-02 10:41:26 +0100

    Don't reinvent "marshal string to JSON" wheel

--- asciicast/frame.go
+++ asciicast/frame.go
@@ -3,7 +3,6 @@ package asciicast
 import (
 	"encoding/json"
 	"fmt"
-	"strconv"
 )
 
 type Frame struct {
@@ -12,8 +11,9 @@ type Frame struct {
 }
 
 func (f *Frame) MarshalJSON() ([]byte, error) {
-	s := fmt.Sprintf(`[%.6f, "%v"]`, f.Delay, rawBytesToEscapedString(f.Data))
-	return []byte(s), nil
+	s, _ := json.Marshal(string(f.Data))
+	json := fmt.Sprintf(`[%.6f, %s]`, f.Delay, s)
+	return []byte(json), nil
 }
 
 func (f *Frame) UnmarshalJSON(data []byte) error {
@@ -32,29 +32,4 @@ func (f *Frame) UnmarshalJSON(data []byte) error {
 	f.Data = b
 
 	return nil
-}
-
-func rawBytesToEscapedString(bytes []byte) string {
-	res := make([]rune, 0)
-
-	for _, runeValue := range string(bytes) {
-		runes := []rune(strconv.Quote(string(runeValue)))
-		runes = runes[1 : len(runes)-1]
-
-		if len(runes) == 4 && runes[0] == '\\' && runes[1] == 'x' {
-			runes = []rune{'\\', 'u', '0', '0', runes[2], runes[3]}
-		}
-
-		if len(runes) == 2 && runes[0] == '\\' && runes[1] == 'a' {
-			runes = []rune{'\\', 'u', '0', '0', '0', '7'}
-		}
-
-		if len(runes) == 2 && runes[0] == '\\' && runes[1] == 'v' {
-			runes = []rune{'\\', 'u', '0', '0', '0', 'b'}
-		}
-
-		res = append(res, runes...)
-	}
-
-	return string(res)
 }
