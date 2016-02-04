27027e565287c6855de7405682d5641e00c625e7
Cole Mickens <cole.mickens@gmail.com>
2016-02-03 22:49:49 -0800

    live feedback on terminal size

--- asciicast/recorder.go
+++ asciicast/recorder.go
@@ -1,8 +1,18 @@
 package asciicast
 
 import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/asciinema/asciinema/terminal"
 	"github.com/asciinema/asciinema/util"
+)
+
+const (
+	optimalCols = 120
+	optimalRows = 30
 )
 
 type Recorder interface {
@@ -17,17 +27,49 @@ func NewRecorder() Recorder {
 	return &AsciicastRecorder{Terminal: terminal.NewTerminal()}
 }
 
+func (r *AsciicastRecorder) checkTerminalSize() chan<- bool {
+	rows, cols, _ := r.Terminal.Size()
+	doneChan := make(chan bool)
+	optimalSize := fmt.Sprintf("%s%dx%d%s", "\x1b[0m\x1b[32m", optimalCols, optimalRows, "\x1b[0m\x1b[33m")
+	go func() {
+		winch := make(chan os.Signal, 1)
+		signal.Notify(winch, syscall.SIGWINCH)
+
+		defer signal.Stop(winch)
+		defer close(winch)
+		defer close(doneChan)
+
+		for {
+			select {
+			case <-winch:
+				newRows, newCols, _ := r.Terminal.Size()
+				if cols != newCols || rows != newRows {
+					cols, rows = newCols, newRows
+					currentSize := fmt.Sprintf("%dx%d", cols, rows)
+					if cols == optimalCols && rows == optimalRows {
+						currentSize = fmt.Sprintf("%s%dx%d%s", "\x1b[0m\x1b[32m", cols, rows, "\x1b[0m\x1b[33m")
+					}
+					util.ReplaceWarningf("Current terminal size is %s. Optimal is %s.", currentSize, optimalSize)
+				}
+			case <-doneChan:
+				return
+			}
+		}
+	}()
+	return doneChan
+}
+
 func (r *AsciicastRecorder) Record(path, command, title string, maxWait uint, assumeYes bool, env map[string]string) error {
 	// TODO: touch savePath to ensure writing is possible
 
 	rows, cols, _ := r.Terminal.Size()
-	if rows > 30 || cols > 120 {
-		util.Warningf("Current terminal size is %vx%v.", cols, rows)
-		util.Warningf("It may be too big to be properly replayed on smaller screens.")
-
+	if rows != optimalRows || cols != optimalCols {
 		if !assumeYes {
+			doneChan := r.checkTerminalSize()
+			util.Warningf("Current terminal size is %vx%v. Optimal is %vx%v", cols, rows, optimalCols, optimalRows)
 			util.Warningf("You can now resize it. Press <Enter> to start recording.")
 			util.ReadLine()
+			doneChan <- true
 		}
 	}
 
