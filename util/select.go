9531a1175fcb3bcf418d3bf1fb47ba3752172f80
Marcin Kulik <m@ku1ik.com>
2016-06-07 21:07:42 +0200

    Bye golang!

--- util/select.go
+++
@@ -1,9 +0,0 @@
-// +build darwin freebsd dragonfly
-
-package util
-
-import "syscall"
-
-func Select(nfd int, r *syscall.FdSet, w *syscall.FdSet, e *syscall.FdSet, timeout *syscall.Timeval) error {
-	return syscall.Select(nfd, r, w, e, timeout)
-}
--- util/select_linux.go
+++
@@ -1,8 +0,0 @@
-package util
-
-import "syscall"
-
-func Select(nfd int, r *syscall.FdSet, w *syscall.FdSet, e *syscall.FdSet, timeout *syscall.Timeval) error {
-	_, err := syscall.Select(nfd, r, w, e, timeout)
-	return err
-}
