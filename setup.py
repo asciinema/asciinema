4960a848b4e0abce44f27635312e58039b115df4
Marcin Kulik <m@ku1ik.com>
2016-07-05 16:09:00 +0200

    Bump development status

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     },
     install_requires=requirements,
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
