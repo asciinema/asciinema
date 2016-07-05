516665588e017ff0432d074a93e4dff2d303d9b2
Marcin Kulik <m@ku1ik.com>
2016-07-05 16:52:08 +0200

    Test signal handling in integration test

--- tests/integration.sh
+++ tests/integration.sh
@@ -4,7 +4,18 @@ set -e
 set -x
 
 python3 -m asciinema -h
+
 python3 -m asciinema --version
+
 python3 -m asciinema auth
-python3 -m asciinema rec -c who __who.json && rm __who.json
-python3 -m asciinema rec -c 'bash -c "echo 1; sleep 1; echo 2"' __sleep.json && rm __sleep.json
+
+python3 -m asciinema rec -c who __tmp.json && rm __tmp.json
+
+bash -c "sleep 1; pkill -28 -f 'thon -m asciinema'" &
+python3 -m asciinema rec -c 'bash -c "echo t3st; sleep 2; echo ok"' __tmp.json && rm __tmp.json
+
+bash -c "sleep 1; pkill -f 'bash -c echo t3st'" &
+python3 -m asciinema rec -c 'bash -c "echo t3st; sleep 2; echo ok"' __tmp.json && rm __tmp.json
+
+bash -c "sleep 1; pkill -9 -f 'bash -c echo t3st'" &
+python3 -m asciinema rec -c 'bash -c "echo t3st; sleep 2; echo ok"' __tmp.json && rm __tmp.json
