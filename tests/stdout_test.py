19243f7c82fb62b0ff8980f60948eebd85accaab
Marcin Kulik <m@ku1ik.com>
2016-07-10 15:49:54 +0200

    Improve Stdout test

--- tests/stdout_test.py
+++ tests/stdout_test.py
@@ -1,19 +1,8 @@
 import time
 
-from nose.tools import assert_equal, assert_raises
+from nose.tools import assert_equal
 from .test_helper import Test, FakeClock
-from asciinema.stdout import Stdout, StdoutTiming
-
-
-class TestStdoutTiming(Test):
-
-    def test_append(self):
-        timing = StdoutTiming()
-
-        timing.append([0.123, 100])
-        timing.append([1234.56, 33])
-
-        assert_equal('0.123000 100\n1234.560000 33', str(timing))
+from asciinema.stdout import Stdout
 
 
 class TestStdout(Test):
@@ -21,24 +10,26 @@ class TestStdout(Test):
     def setUp(self):
         Test.setUp(self)
         self.real_time = time.time
-        time.time = FakeClock([1, 3, 10]).time
+        time.time = FakeClock([1, 3, 10, 13, 17]).time
 
     def tearDown(self):
         time.time = self.real_time
 
     def test_write(self):
-        timing = []
-        stdout = Stdout(timing)
+        stdout = Stdout()
 
         stdout.write(b'foo')
         stdout.write(b'barbaz')
+        stdout.write('żó'.encode('utf-8') + bytes([0xc5]))
+        stdout.write(bytes([0x82]) + 'ć'.encode('utf-8'))
 
-        assert_equal(b'foobarbaz', stdout.data)
-        assert_equal([[2, 3], [7, 6]], timing)
+        assert_equal([[2, 'foo'], [7, 'barbaz'], [3, 'żó'], [4, 'łć']], stdout.frames)
 
     def test_close(self):
         stdout = Stdout()
 
+        stdout.write(b'foo')
+        stdout.write(b'barbaz')
         stdout.close()
 
-        assert_raises(ValueError, stdout.write, 'qux')
+        assert_equal(12, stdout.duration)
