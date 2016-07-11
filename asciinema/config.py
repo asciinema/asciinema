55e738af5d794f308399403410676537829237cf
Marcin Kulik <m@ku1ik.com>
2016-07-11 20:42:27 +0200

    Fix config dir creation on Python <= 3.3

--- asciinema/config.py
+++ asciinema/config.py
@@ -68,7 +68,10 @@ def create_file(filename):
     config = configparser.ConfigParser()
     config['api'] = {}
     config['api']['token'] = str(uuid.uuid4())
-    os.makedirs(path.dirname(filename), exist_ok=True)
+
+    if not path.exists(path.dirname(filename)):
+        os.makedirs(path.dirname(filename))
+
     with open(filename, 'w') as f:
         config.write(f)
 
