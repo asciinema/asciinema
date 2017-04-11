479cfafecba2595332113ce18b421bc4955b1ad4
Marcin Kulik <m@ku1ik.com>
2017-04-11 17:15:17 +0200

    Eat all stdin data during playback ONLY IF stdin is a TTY

--- asciinema/player.py
+++ asciinema/player.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import time
 
@@ -7,14 +8,21 @@ from asciinema.term import raw, read_non_blocking
 class Player:
 
     def play(self, asciicast, max_wait=None, speed=1.0):
-        with raw(sys.stdin.fileno()):
-            for delay, text in asciicast.stdout:
-                if max_wait and delay > max_wait:
-                    delay = max_wait
-                time.sleep(delay / speed)
-                sys.stdout.write(text)
-                sys.stdout.flush()
+        if os.isatty(sys.stdin.fileno()):
+            with raw(sys.stdin.fileno()):
+                self._play(asciicast, max_wait, speed, True)
+        else:
+            self._play(asciicast, max_wait, speed, False)
 
+    def _play(self, asciicast, max_wait, speed, raw):
+        for delay, text in asciicast.stdout:
+            if max_wait and delay > max_wait:
+                delay = max_wait
+            time.sleep(delay / speed)
+            sys.stdout.write(text)
+            sys.stdout.flush()
+
+            if raw:
                 data = read_non_blocking(sys.stdin.fileno())
                 if 0x03 in data:  # ctrl-c
                     break
