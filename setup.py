1720b2de862cedcfd976824a9efaf8c56118a1e7
Marcin Kulik <m@ku1ik.com>
2017-04-11 17:00:38 +0200

    Update pypi classifiers

--- setup.py
+++ setup.py
@@ -33,10 +33,10 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: System :: Shells',
         'Topic :: Terminals',
         'Topic :: Utilities'
