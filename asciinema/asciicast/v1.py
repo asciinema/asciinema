847ad6d141db3d15554b0521a50182bfafbe3641
Marcin Kulik <m@ku1ik.com>
2017-09-03 22:13:24 +0200

    Fix pep-8 warnings

--- asciinema/asciicast/v1.py
+++ asciinema/asciicast/v1.py
@@ -3,7 +3,7 @@ class Asciicast:
     def __init__(self, stdout):
         self.version = 1
         self.__stdout = stdout
-        self.max_wait = None # v1 doesn't store it
+        self.max_wait = None  # v1 doesn't store it
 
     def stdout(self):
         return self.__stdout
