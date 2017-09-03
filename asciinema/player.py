2a1874d37e6f282fdbf8382b4fdf2beee5109710
Marcin Kulik <m@ku1ik.com>
2017-09-03 20:46:03 +0200

    Support of v2 format in `asciinema play`

--- asciinema/player.py
+++ asciinema/player.py
@@ -5,6 +5,17 @@ import time
 from asciinema.term import raw, read_non_blocking
 
 
+def compress_time(stdout, max_wait):
+    if max_wait:
+        return ([min(delay, max_wait), text] for delay, text in stdout)
+    else:
+        return stdout
+
+
+def adjust_speed(stdout, speed):
+    return ([delay / speed, text] for delay, text in stdout)
+
+
 class Player:
 
     def play(self, asciicast, max_wait=None, speed=1.0):
@@ -15,10 +26,14 @@ class Player:
             self._play(asciicast, max_wait, speed, False)
 
     def _play(self, asciicast, max_wait, speed, raw):
-        for delay, text in asciicast.stdout:
-            if max_wait and delay > max_wait:
-                delay = max_wait
-            time.sleep(delay / speed)
+        max_wait = max_wait or asciicast.max_wait
+
+        stdout = asciicast.stdout()
+        stdout = compress_time(stdout, max_wait)
+        stdout = adjust_speed(stdout, speed)
+
+        for delay, text in stdout:
+            time.sleep(delay)
             sys.stdout.write(text)
             sys.stdout.flush()
 
