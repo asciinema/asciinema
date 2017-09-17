7f759a902b15c08cf6158238190bca2818c7c0a7
Kelvin Ly <kelvin.ly1618@gmail.com>
2017-09-17 02:24:21 -0400

    Add step control

--- asciinema/player.py
+++ asciinema/player.py
@@ -15,9 +15,12 @@ class Player:
             self._play(asciicast, max_wait, speed, False)
 
     def _play(self, asciicast, max_wait, speed, raw):
+        step = False
         for delay, text in asciicast.stdout:
             if max_wait and delay > max_wait:
                 delay = max_wait
+            if step:
+                delay = 0
             time.sleep(delay / speed)
             sys.stdout.write(text)
             sys.stdout.flush()
@@ -26,8 +29,9 @@ class Player:
                 data = read_non_blocking(sys.stdin.fileno())
                 if 0x03 in data:  # ctrl-c
                     break
-                if 0x20 in data:  # space
-                    exit = self._pause_loop()
+                if 0x20 in data or step:  # space
+                    step = False
+                    step, exit = self._pause_loop()
                     if exit:
                         break
                 if 0x2b in data:  # plus sign
@@ -39,7 +43,9 @@ class Player:
         while True:
             time.sleep(0.001)
             paused_data = read_non_blocking(sys.stdin.fileno())
+            if 0x2e in paused_data:  # period; step one terminal change
+                return True, False
             if 0x20 in paused_data:
-                return False
+                return False, False
             if 0x03 in paused_data:  # ctrl-c
-                return True
+                return False, True
