f517ee92b1c5623305c63d3564276d0206c04908
Marcin Kulik <m@ku1ik.com>
2017-12-02 19:00:53 +0100

    Tweak test output

--- tests/distros.sh
+++ tests/distros.sh
@@ -7,6 +7,8 @@ tests_dir="$(cd "$(dirname "$path_to_self")" && pwd)"
 
 test() {
     printf "\e[1;32mTesting on $1...\e[0m\n"
+    echo
+
     docker build -t asciinema/asciinema:$1 -f tests/distros/Dockerfile.$1 .
     docker run --rm -ti asciinema/asciinema:$1 tests/integration.sh
 }
@@ -15,3 +17,6 @@ test ubuntu
 test debian
 test fedora
 test centos
+
+echo
+printf "\e[1;32mAll tests passed.\e[0m\n"
