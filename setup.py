accc117049fa1003d7c98e405cfb7c4232150201
Marcin Kulik <m@ku1ik.com>
2019-01-12 19:54:59 +0100

    Add PyPi tag for Python 3.7

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: System :: Shells',
         'Topic :: Terminals',
         'Topic :: Utilities'
