b0bdcdb30e6efffda2b28a6e418d89e13f4ae543
zurgeg <zurgeg@gmail.com>
2021-07-15 10:27:34 -0400

    Delete something I was using for reference

--- asciinema/fcntl_stub.py
+++ asciinema/fcntl_stub.py
@@ -1,6 +1,3 @@
-    flags = fcntl.fcntl(pipe_w, fcntl.F_GETFL, 0)
-    flags = flags | os.O_NONBLOCK
-    flags = fcntl.fcntl(pipe_w, fcntl.F_SETFL, flags)
 def fcntl(*args):
   return 1
 F_GETFL = 1
