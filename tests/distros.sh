1f8bfb7c680acc445e61932caf1d25519419415c
Davis Schirmer <djds@bghost.xyz>
2021-10-29 23:04:09 -0400

    [test] Shellcheck tests/*.sh

--- tests/distros.sh
+++ tests/distros.sh
@@ -2,21 +2,25 @@
 
 set -e
 
-path_to_self="${BASH_SOURCE[0]}"
-tests_dir="$(cd "$(dirname "$path_to_self")" && pwd)"
 
-test() {
-    printf "\e[1;32mTesting on $1...\e[0m\n"
-    echo
+# do not redefine builtin `test`
+test_() {
+    local -r tag="${1}"
 
-    docker build -t asciinema/asciinema:$1 -f tests/distros/Dockerfile.$1 .
-    docker run --rm -ti asciinema/asciinema:$1 tests/integration.sh
+    printf "\e[1;32mTesting on %s...\e[0m\n\n" "${tag}"
+
+    docker build \
+        --tag="asciinema/asciinema:${tag}" \
+        --file="tests/distros/Dockerfile.${tag}" \
+        .
+
+    docker run --rm -i "asciinema/asciinema:${tag}" tests/integration.sh
 }
 
-test ubuntu
-test debian
-test fedora
-test centos
 
-echo
-printf "\e[1;32mAll tests passed.\e[0m\n"
+test_ ubuntu
+test_ debian
+test_ fedora
+test_ centos
+
+printf "\n\e[1;32mAll tests passed.\e[0m\n"
