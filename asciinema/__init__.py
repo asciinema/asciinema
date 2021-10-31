ca05ba934f8872371f6556bffc48217c594d52ae
Davis Schirmer <djds@bghost.xyz>
2021-10-31 17:49:39 -0400

    [typing] Annotate asciinema.asciicast

--- asciinema/__init__.py
+++ asciinema/__init__.py
@@ -1,19 +1,30 @@
 import sys
 
-__author__ = 'Marcin Kulik'
-__version__ = '2.0.2'
+__author__ = "Marcin Kulik"
+__version__ = "2.0.2"
 
-if sys.version_info[0] < 3:
-    raise ImportError('Python < 3 is unsupported.')
+if sys.version_info < (3, 7):
+    raise ImportError("Python < 3.7 is unsupported.")
 
-import asciinema.recorder
+# pylint: disable=wrong-import-position
+from typing import Any
+
+from .recorder import record
 
 
-def record_asciicast(path, command=None, append=False, idle_time_limit=None,
-                     rec_stdin=False, title=None, metadata=None,
-                     command_env=None, capture_env=None):
-    asciinema.recorder.record(
-        path,
+def record_asciicast(
+    path_,
+    command=None,
+    append=False,
+    idle_time_limit=None,
+    rec_stdin=False,
+    title=None,
+    metadata: Any = None,
+    command_env: Any = None,
+    capture_env: Any = None,
+) -> None:
+    record(
+        path_,
         command=command,
         append=append,
         idle_time_limit=idle_time_limit,
@@ -21,5 +32,5 @@ def record_asciicast(path, command=None, append=False, idle_time_limit=None,
         title=title,
         metadata=metadata,
         command_env=command_env,
-        capture_env=capture_env
+        capture_env=capture_env,
     )
--- asciinema/asciicast/v2.py
+++ asciinema/asciicast/v2.py
@@ -1,11 +1,11 @@
+from __future__ import annotations
+
 import codecs
 import json
-import json.decoder
-
-try:
-    JSONDecodeError = json.decoder.JSONDecodeError
-except AttributeError:
-    JSONDecodeError = ValueError
+from codecs import StreamReader
+from io import IOBase
+from json.decoder import JSONDecodeError
+from typing import IO, Any, Dict, Generator, List, Optional, TextIO, Union
 
 
 class LoadError(Exception):
@@ -13,88 +13,97 @@ class LoadError(Exception):
 
 
 class Asciicast:
-    def __init__(self, f, header):
-        self.version = 2
+    def __init__(
+        self, f: Union[TextIO, StreamReader], header: Dict[str, Any]
+    ) -> None:
+        self.version: int = 2
         self.__file = f
         self.v2_header = header
         self.idle_time_limit = header.get("idle_time_limit")
 
-    def events(self):
+    def events(self) -> Generator[Any, None, None]:
         for line in self.__file:
             yield json.loads(line)
 
-    def stdout_events(self):
-        for time, type, data in self.events():
-            if type == "o":
-                yield [time, type, data]
+    def stdout_events(self) -> Generator[List[Any], None, None]:
+        for time, type_, data in self.events():
+            if type_ == "o":
+                yield [time, type_, data]
 
 
-def build_from_header_and_file(header, f):
+def build_from_header_and_file(
+    header: Dict[str, Any], f: Union[StreamReader, TextIO]
+) -> Asciicast:
     return Asciicast(f, header)
 
 
 class open_from_file:
     FORMAT_ERROR = "only asciicast v2 format can be opened"
 
-    def __init__(self, first_line, file):
+    def __init__(
+        self, first_line: str, file: Union[StreamReader, TextIO]
+    ) -> None:
         self.first_line = first_line
         self.file = file
 
-    def __enter__(self):
+    def __enter__(self) -> Optional[Asciicast]:
         try:
-            v2_header = json.loads(self.first_line)
+            v2_header: Dict[str, Any] = json.loads(self.first_line)
             if v2_header.get("version") == 2:
                 return build_from_header_and_file(v2_header, self.file)
-            else:
-                raise LoadError(self.FORMAT_ERROR)
-        except JSONDecodeError:
             raise LoadError(self.FORMAT_ERROR)
+        except JSONDecodeError as e:
+            raise LoadError(self.FORMAT_ERROR) from e
 
-    def __exit__(self, exc_type, exc_value, exc_traceback):
+    def __exit__(
+        self, exc_type: str, exc_value: str, exc_traceback: str
+    ) -> None:
         self.file.close()
 
 
-def get_duration(path):
-    with open(path, mode="rt", encoding="utf-8") as f:
+def get_duration(path_: str) -> Any:
+    with open(path_, mode="rt", encoding="utf-8") as f:
         first_line = f.readline()
         with open_from_file(first_line, f) as a:
+            assert isinstance(a, Asciicast)
+            last_frame = None
             for last_frame in a.stdout_events():
                 pass
             return last_frame[0]
 
 
-def build_header(width, height, metadata):
+def build_header(
+    width: Optional[int], height: Optional[int], metadata: Any
+) -> Dict[str, Any]:
     header = {"version": 2, "width": width, "height": height}
     header.update(metadata)
 
     assert "width" in header, "width missing in metadata"
     assert "height" in header, "height missing in metadata"
-    assert type(header["width"]) == int
-    assert type(header["height"]) == int
+    assert isinstance(header["width"], int)
+    assert isinstance(header["height"], int)
 
     if "timestamp" in header:
-        assert (
-            type(header["timestamp"]) == int
-            or type(header["timestamp"]) == float
-        )
+        assert isinstance(header["timestamp"], (int, float))
 
     return header
 
 
 class writer:
-    def __init__(
+    def __init__(  # pylint: disable=too-many-arguments
         self,
-        path,
-        metadata=None,
-        append=False,
-        buffering=1,
-        width=None,
-        height=None,
-    ):
+        path: str,
+        metadata: Any = None,
+        append: bool = False,
+        buffering: int = 1,
+        width: Optional[int] = None,
+        height: Optional[int] = None,
+    ) -> None:
         self.path = path
         self.buffering = buffering
         self.stdin_decoder = codecs.getincrementaldecoder("UTF-8")("replace")
         self.stdout_decoder = codecs.getincrementaldecoder("UTF-8")("replace")
+        self.file: Optional[IO[Any]] = None
 
         if append:
             self.mode = "a"
@@ -103,34 +112,43 @@ class writer:
             self.mode = "w"
             self.header = build_header(width, height, metadata or {})
 
-    def __enter__(self):
-        self.file = open(self.path, mode=self.mode, buffering=self.buffering)
+    def __enter__(self) -> writer:
+        self.file = open(
+            self.path,
+            mode=self.mode,
+            buffering=self.buffering,
+            encoding="utf-8",
+        )
 
         if self.header:
             self.__write_line(self.header)
 
         return self
 
-    def __exit__(self, exc_type, exc_value, exc_traceback):
+    def __exit__(
+        self, exc_type: str, exc_value: str, exc_traceback: str
+    ) -> None:
+        assert isinstance(self.file, IOBase)
         self.file.close()
 
-    def write_stdout(self, ts, data):
-        if type(data) == str:
+    def write_stdout(self, ts: float, data: Union[str, bytes]) -> None:
+        if isinstance(data, str):
             data = data.encode(encoding="utf-8", errors="strict")
         data = self.stdout_decoder.decode(data)
         self.__write_event(ts, "o", data)
 
-    def write_stdin(self, ts, data):
-        if type(data) == str:
+    def write_stdin(self, ts: float, data: Union[str, bytes]) -> None:
+        if isinstance(data, str):
             data = data.encode(encoding="utf-8", errors="strict")
         data = self.stdin_decoder.decode(data)
         self.__write_event(ts, "i", data)
 
-    def __write_event(self, ts, etype, data):
+    def __write_event(self, ts: float, etype: str, data: str) -> None:
         self.__write_line([round(ts, 6), etype, data])
 
-    def __write_line(self, obj):
+    def __write_line(self, obj: Any) -> None:
         line = json.dumps(
             obj, ensure_ascii=False, indent=None, separators=(", ", ": ")
         )
-        self.file.write(line + "\n")
+        assert isinstance(self.file, IOBase)
+        self.file.write(f"{line}\n")
