45c065e28dd2a1353c1554233bd5ab8fab1a3e6b
Davis Schirmer <djds@bghost.xyz>
2021-11-03 02:17:04 -0400

    [typing] Annotate asciinema.recorder

--- asciinema/recorder.py
+++ asciinema/recorder.py
@@ -1,32 +1,37 @@
 import os
 import time
+from typing import Any, Callable, Dict, Optional, Tuple, Type
 
-from . import pty_ as pty  # avoid
+from . import pty_ as pty  # avoid collisions with standard library `pty`
 from . import term
 from .asciicast import v2
+from .asciicast.v2 import writer as w2
 from .async_worker import async_worker
 
 
-def record(
-    path,
-    command=None,
-    append=False,
-    idle_time_limit=None,
-    rec_stdin=False,
-    title=None,
-    metadata=None,
-    command_env=None,
-    capture_env=None,
-    writer=v2.writer,
-    record=pty.record,
-    notifier=None,
-    key_bindings={},
-):
+def record(  # pylint: disable=too-many-arguments,too-many-locals
+    path_: str,
+    command: Any = None,
+    append: bool = False,
+    idle_time_limit: Optional[int] = None,
+    rec_stdin: bool = False,
+    title: Optional[str] = None,
+    metadata: Any = None,
+    command_env: Optional[Dict[Any, Any]] = None,
+    capture_env: Any = None,
+    writer: Type[w2] = v2.writer,
+    record_: Callable[..., None] = pty.record,
+    notifier: Any = None,
+    key_bindings: Optional[Dict[str, Any]] = None,
+) -> None:
     if command is None:
-        command = os.environ.get("SHELL") or "sh"
+        command = os.environ.get("SHELL", "sh")
 
     if command_env is None:
         command_env = os.environ.copy()
+
+    if key_bindings is None:
+        key_bindings = {}
 
     command_env["ASCIINEMA_REC"] = "1"
 
@@ -35,7 +40,11 @@ def record(
 
     w, h = term.get_size()
 
-    full_metadata = {"width": w, "height": h, "timestamp": int(time.time())}
+    full_metadata: Dict[str, Any] = {
+        "width": w,
+        "height": h,
+        "timestamp": int(time.time()),
+    }
 
     full_metadata.update(metadata or {})
 
@@ -50,43 +59,48 @@ def record(
     if title:
         full_metadata["title"] = title
 
-    time_offset = 0
+    time_offset: float = 0
 
-    if append and os.stat(path).st_size > 0:
-        time_offset = v2.get_duration(path)
+    if append and os.stat(path_).st_size > 0:
+        assert time_offset is not None
+        time_offset = v2.get_duration(path_)
 
-    with async_writer(writer, path, full_metadata, append) as w:
-        with async_notifier(notifier) as n:
-            record(
+    with async_writer(writer, path_, full_metadata, append) as _writer:
+        with async_notifier(notifier) as _notifier:
+            record_(
                 ["sh", "-c", command],
-                w,
+                _writer,
                 command_env,
                 rec_stdin,
                 time_offset,
-                n,
+                _notifier,
                 key_bindings,
             )
 
 
 class async_writer(async_worker):
-    def __init__(self, writer, path, metadata, append=False):
+    def __init__(
+        self, writer: Type[w2], path_: str, metadata: Any, append: bool = False
+    ) -> None:
         async_worker.__init__(self)
         self.writer = writer
-        self.path = path
+        self.path = path_
         self.metadata = metadata
         self.append = append
 
-    def write_stdin(self, ts, data):
+    def write_stdin(self, ts: float, data: Any) -> None:
         self.enqueue([ts, "i", data])
 
-    def write_stdout(self, ts, data):
+    def write_stdout(self, ts: float, data: Any) -> None:
         self.enqueue([ts, "o", data])
 
-    def run(self):
+    def run(self) -> None:
         with self.writer(
             self.path, metadata=self.metadata, append=self.append
         ) as w:
+            event: Tuple[float, str, Any]
             for event in iter(self.queue.get, None):
+                assert event is not None
                 ts, etype, data = event
 
                 if etype == "o":
@@ -96,18 +110,18 @@ class async_writer(async_worker):
 
 
 class async_notifier(async_worker):
-    def __init__(self, notifier):
+    def __init__(self, notifier: Any) -> None:
         async_worker.__init__(self)
         self.notifier = notifier
 
-    def notify(self, text):
+    def notify(self, text: str) -> None:
         self.enqueue(text)
 
-    def perform(self, text):
+    def perform(self, text: str) -> None:
         try:
             if self.notifier:
                 self.notifier.notify(text)
-        except:
+        except:  # pylint: disable=bare-except # noqa: E722
             # we catch *ALL* exceptions here because we don't want failed
             # notification to crash the recording session
             pass
