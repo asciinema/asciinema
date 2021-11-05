ecb2b60568573fffca750b6a882e727d931163fd
Davis Schirmer <djds@bghost.xyz>
2021-11-05 00:37:34 -0400

    [typing] Annotate asciinema.commands.cat

--- asciinema/commands/cat.py
+++ asciinema/commands/cat.py
@@ -1,23 +1,25 @@
 import sys
+from typing import Any, Dict
 
-import asciinema.asciicast as asciicast
-from asciinema.commands.command import Command
-from asciinema.term import raw
+from .. import asciicast
+from ..config import Config
+from ..term import raw
+from .command import Command
 
 
 class CatCommand(Command):
-    def __init__(self, args, config, env):
+    def __init__(self, args: Any, config: Config, env: Dict[str, str]):
         Command.__init__(self, args, config, env)
         self.filename = args.filename
 
-    def execute(self):
+    def execute(self) -> int:
         try:
-            stdin = open("/dev/tty")
-            with raw(stdin.fileno()):
-                with asciicast.open_from_url(self.filename) as a:
-                    for t, _type, text in a.stdout_events():
-                        sys.stdout.write(text)
-                        sys.stdout.flush()
+            with open("/dev/tty", "wt", encoding="utf-8") as stdin:
+                with raw(stdin.fileno()):
+                    with asciicast.open_from_url(self.filename) as a:
+                        for _, _type, text in a.stdout_events():
+                            sys.stdout.write(text)
+                            sys.stdout.flush()
 
         except asciicast.LoadError as e:
             self.print_error(f"printing failed: {str(e)}")
