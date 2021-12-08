8f8d524957381d8156d0693226d6c68bcc997e9f
Charalampos Stratakis <cstratak@redhat.com>
2021-12-08 02:51:51 +0100

    Remove unused code

--- asciinema/tests/test_helper.py
+++ asciinema/tests/test_helper.py
@@ -9,16 +9,6 @@ import unittest
 stdout = None
 
 
-def assert_printed(expected):
-    success = expected in stdout.getvalue()
-    assert success, 'expected text "%s" not printed' % expected
-
-
-def assert_not_printed(expected):
-    success = expected not in stdout.getvalue()
-    assert success, 'not expected text "%s" printed' % expected
-
-
 class Test(unittest.TestCase):
 
     def setUp(self):
@@ -28,25 +18,3 @@ class Test(unittest.TestCase):
 
     def tearDown(self):
         sys.stdout = self.real_stdout
-
-
-class FakeClock:
-
-    def __init__(self, values):
-        self.values = values
-        self.n = 0
-
-    def time(self):
-        value = self.values[self.n]
-        self.n += 1
-
-        return value
-
-
-class FakeAsciicast:
-
-    def __init__(self, cmd=None, title=None, stdout=None, meta_data=None):
-        self.cmd = cmd
-        self.title = title
-        self.stdout = stdout
-        self.meta_data = meta_data or {}
