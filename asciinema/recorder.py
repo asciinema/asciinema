760a7152e24962f255311b0ed57a4d33cc9b85ee
Marcin Kulik <m@ku1ik.com>
2022-02-17 22:52:39 +0100

    Simplify async_writer

--- asciinema/recorder.py
+++ asciinema/recorder.py
@@ -71,13 +71,11 @@ def record(  # pylint: disable=too-many-arguments,too-many-locals
         time_offset = v2.get_duration(path_)
 
     with async_notifier(notifier) as _notifier:
-        with async_writer(
-            writer,
-            path_,
-            full_metadata,
-            append,
-            _notifier.queue,
-        ) as _writer:
+        sync_writer = writer(
+            path_, full_metadata, append, on_error=_notifier.queue.put
+        )
+
+        with async_writer(sync_writer) as _writer:
             record_(
                 ["sh", "-c", command],
                 _writer,
@@ -93,20 +91,9 @@ def record(  # pylint: disable=too-many-arguments,too-many-locals
 
 
 class async_writer(async_worker):
-    def __init__(
-        self,
-        writer: Type[w2],
-        path_: str,
-        metadata: Any,
-        append: bool = False,
-        notifier_q: Any = None,
-    ) -> None:
+    def __init__(self, writer: w2) -> None:
         async_worker.__init__(self)
         self.writer = writer
-        self.path = path_
-        self.metadata = metadata
-        self.append = append
-        self.notifier_q = notifier_q
 
     def write_stdin(self, ts: float, data: Any) -> None:
         self.enqueue([ts, "i", data])
@@ -115,12 +102,7 @@ class async_writer(async_worker):
         self.enqueue([ts, "o", data])
 
     def run(self) -> None:
-        with self.writer(
-            self.path,
-            metadata=self.metadata,
-            append=self.append,
-            on_error=self.__on_error,
-        ) as w:
+        with self.writer as w:
             event: Tuple[float, str, Any]
             for event in iter(self.queue.get, None):
                 assert event is not None
@@ -130,10 +112,6 @@ class async_writer(async_worker):
                     w.write_stdout(ts, data)
                 elif etype == "i":
                     w.write_stdin(ts, data)
-
-    def __on_error(self, reason: str) -> None:
-        if self.notifier_q:
-            self.notifier_q.put(reason)
 
 
 class async_notifier(async_worker):
