2fe0584450aaacd2f9873f746e8885ef74e05a5f
Marcin Kulik <m@ku1ik.com>
2022-04-10 17:05:12 +0200

    DRY-up color handling in Command.print* methods

--- asciinema/commands/command.py
+++ asciinema/commands/command.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 from ..api import Api
 from ..config import Config
@@ -15,28 +15,20 @@ class Command:
         self,
         text: str,
         end: str = "\n",
+        color: Optional[int] = None,
         force: bool = False,
     ) -> None:
         if not self.quiet or force:
+            if color is not None and os.isatty(sys.stderr.fileno()):
+                text = f"\x1b[0;3{color}m{text}\x1b[0m"
+
             print(text, file=sys.stderr, end=end)
 
     def print_info(self, text: str) -> None:
-        if os.isatty(sys.stderr.fileno()):
-            self.print(f"\x1b[0;32masciinema: {text}\x1b[0m")
-        else:
-            self.print(f"asciinema: {text}")
+        self.print(f"asciinema: {text}", color=2)
 
     def print_warning(self, text: str) -> None:
-        if os.isatty(sys.stderr.fileno()):
-            self.print(f"\x1b[0;33masciinema: {text}\x1b[0m")
-        else:
-            self.print(f"asciinema: {text}")
+        self.print(f"asciinema: {text}", color=3)
 
     def print_error(self, text: str) -> None:
-        if os.isatty(sys.stderr.fileno()):
-            self.print(
-                f"\x1b[0;31masciinema: {text}\x1b[0m",
-                force=True,
-            )
-        else:
-            self.print(f"asciinema: {text}", force=True)
+        self.print(f"asciinema: {text}", color=1, force=True)
