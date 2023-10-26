644e24b33e95cd2f1b409f4bb6dd045ed568b574
Marcin Kulik <m@ku1ik.com>
2023-10-26 17:30:42 +0200

    Bring clap, define rec subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,97 @@
 mod asciicast;
 mod pty;
 mod recorder;
-use anyhow::{anyhow, Result};
-use std::env;
+use anyhow::Result;
+use clap::{Args, Parser, Subcommand, ValueEnum};
+
+#[derive(Debug, Parser)]
+#[command(name = "asciinema")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Record terminal session
+    #[command(name = "rec")]
+    Record {
+        filename: String,
+
+        /// Enable input recording
+        #[arg(long)]
+        stdin: bool,
+
+        /// Append to existing asciicast file
+        #[arg(long)]
+        append: bool,
+
+        /// Save raw output only
+        #[arg(long)]
+        raw: bool,
+
+        /// Overwrite target file if it already exists
+        #[arg(long)]
+        overwrite: bool,
+
+        /// Command to record
+        #[arg(short, long, default_value_t = String::from("$SHELL"))]
+        command: String,
+
+        /// List of env vars to save
+        #[arg(short, long, default_value_t = String::from("SHELL,TERM"))]
+        env: String,
+
+        /// Title of the recording
+        #[arg(short, long)]
+        title: Option<String>,
+
+        /// Limit idle time to given number of seconds
+        #[arg(short, long, value_name = "SECS")]
+        idle_time_limit: Option<f64>,
+
+        /// Override terminal width (columns) for recorded command
+        #[arg(long)]
+        cols: Option<u16>,
+
+        /// Override terminal height (rows) for recorded command
+        #[arg(long)]
+        rows: Option<u16>,
+
+        /// Quiet mode - suppress all notices/warnings
+        #[arg(short, long)]
+        quiet: bool,
+    },
+}
 
 fn main() -> Result<()> {
-    let path = env::args()
-        .nth(1)
-        .ok_or(anyhow!("output filename missing"))?;
+    let cli = Cli::parse();
 
-    let mut recorder = recorder::new(path, recorder::Format::Asciicast, false, true)?;
-    pty::exec(&["/bin/bash"], &mut recorder)?;
+    match cli.command {
+        Commands::Record {
+            filename,
+            stdin,
+            append,
+            raw,
+            overwrite,
+            command,
+            env,
+            title,
+            idle_time_limit,
+            cols,
+            rows,
+            quiet,
+        } => {
+            let format = if raw {
+                recorder::Format::Raw
+            } else {
+                recorder::Format::Asciicast
+            };
+
+            let mut recorder = recorder::new(filename, format, append, stdin)?;
+            pty::exec(&["/bin/bash"], &mut recorder)?;
+        }
+    }
 
     Ok(())
 }
