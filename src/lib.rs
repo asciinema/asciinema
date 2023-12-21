7005e813b82af732d8fbeebcc1932fcff1cfeefb
Marcin Kulik <m@ku1ik.com>
2023-12-21 18:17:03 +0100

    New implementation of auth command

--- src/lib.rs
+++ src/lib.rs
@@ -59,3 +59,16 @@ fn build_user_agent() -> String {
 
     ua.to_owned()
 }
+
+pub fn auth(server_url: String) -> Result<()> {
+    let mut auth_url = Url::parse(&server_url)?;
+    let install_id = util::get_install_id()?;
+    auth_url.set_path(&format!("connect/{install_id}"));
+    let server_hostname = auth_url.host().ok_or(anyhow!("invalid server URL"))?;
+
+    println!("Open the following URL in a web browser to authenticate this asciinema CLI with your {server_hostname} user account:\n");
+    println!("{}\n", auth_url);
+    println!("This action will associate all recordings uploaded from this machine (past and future ones) with your account, allowing you to manage them (change the title/theme, delete) at {server_hostname}.");
+
+    Ok(())
+}
