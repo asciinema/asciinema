e93928be4651b85773993441b28dffa9d44e5b07
Marcin Kulik <m@ku1ik.com>
2024-01-02 12:23:01 +0100

    New implementation of cat command

--- src/cmd/cat.rs
+++ src/cmd/cat.rs
@@ -1,5 +1,7 @@
+use crate::format::asciicast;
 use anyhow::Result;
 use clap::Args;
+use std::{fs, io};
 
 #[derive(Debug, Args)]
 pub struct Cli {
@@ -9,6 +11,30 @@ pub struct Cli {
 
 impl Cli {
     pub fn run(self) -> Result<()> {
-        todo!();
+        let mut writer = asciicast::Writer::new(io::stdout(), 0);
+        let mut time_offset: u64 = 0;
+        let mut first = true;
+
+        for path in self.filename.iter() {
+            let reader = io::BufReader::new(fs::File::open(path)?);
+            let (header, events) = asciicast::open(reader)?;
+            let mut time = time_offset;
+
+            if first {
+                writer.write_header(&header)?;
+                first = false;
+            }
+
+            for event in events {
+                let mut event = event?;
+                time = time_offset + event.time;
+                event.time = time;
+                writer.write_event(event)?;
+            }
+
+            time_offset = time;
+        }
+
+        Ok(())
     }
 }
