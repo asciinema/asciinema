3f9ffd0254cef40d58e0fae432a581965611f346
Marcin Kulik <m@ku1ik.com>
2024-01-23 21:53:24 +0100

    Fix parsing of round numbers in deserialize_time

--- src/asciicast/util.rs
+++ src/asciicast/util.rs
@@ -8,8 +8,13 @@ where
     use serde::de::Error;
 
     let value: serde_json::Value = Deserialize::deserialize(deserializer)?;
-    let string = value.as_f64().map(|v| v.to_string()).unwrap_or_default();
-    let parts: Vec<&str> = string.split('.').collect();
+
+    let number = value
+        .as_f64()
+        .map(|v| v.to_string())
+        .ok_or(Error::custom("expected number"))?;
+
+    let parts: Vec<&str> = number.split('.').collect();
 
     match parts.as_slice() {
         [left, right] => {
@@ -23,6 +28,12 @@ where
             Ok(secs * 1_000_000 + micros)
         }
 
-        _ => Err(Error::custom("invalid time format")),
+        [number] => {
+            let secs: u64 = number.parse().map_err(Error::custom)?;
+
+            Ok(secs * 1_000_000)
+        }
+
+        _ => Err(Error::custom(format!("invalid time format: {value}"))),
     }
 }
