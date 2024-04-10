b4d0cbd878bbf175e8311ca2265fc20d1e52e024
Marcin Kulik <m@ku1ik.com>
2024-04-10 10:42:01 +0200

    Update diag message for stream client count

--- src/streamer/mod.rs
+++ src/streamer/mod.rs
@@ -223,7 +223,7 @@ async fn event_loop(
                 match client {
                     Some(client) => {
                         client.accept(session.subscribe());
-                        info!("viewer count: {}", session.subscriber_count());
+                        info!("client count: {}", session.subscriber_count());
                     }
 
                     None => break,
