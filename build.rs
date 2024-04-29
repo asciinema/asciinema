2c178c82a6afce676090b816b595465bda29cf98
Marcin Kulik <m@ku1ik.com>
2024-04-29 21:52:47 +0200

    Generate shell auto-completion files at build time

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 use clap::CommandFactory;
+use clap::ValueEnum;
+use clap_complete::{generate_to, Shell};
 use clap_mangen::Man;
 use std::env;
 use std::fs::File;
@@ -11,9 +13,14 @@ mod cli {
 
 fn main() -> std::io::Result<()> {
     let out_dir = PathBuf::from(env::var_os("OUT_DIR").ok_or(io::ErrorKind::NotFound)?);
-    let cmd = cli::Cli::command();
-    let man = Man::new(cmd);
+    let mut cmd = cli::Cli::command();
+
+    let man = Man::new(cmd.clone());
     man.render(&mut File::create(out_dir.join("asciinema.1"))?)?;
+
+    for shell in Shell::value_variants() {
+        generate_to(*shell, &mut cmd, "asciinema", &out_dir).unwrap();
+    }
 
     Ok(())
 }
