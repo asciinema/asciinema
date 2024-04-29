3e73959c06ca20bacb819083dc4755f5f029bbf3
Marcin Kulik <m@ku1ik.com>
2024-04-29 21:42:23 +0200

    Render man page without a buffer

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use clap::CommandFactory;
 use clap_mangen::Man;
 use std::env;
-use std::fs;
+use std::fs::File;
 use std::io;
 use std::path::PathBuf;
 
@@ -13,8 +13,7 @@ fn main() -> std::io::Result<()> {
     let out_dir = PathBuf::from(env::var_os("OUT_DIR").ok_or(io::ErrorKind::NotFound)?);
     let cmd = cli::Cli::command();
     let man = Man::new(cmd);
-    let mut buffer: Vec<u8> = Default::default();
-    man.render(&mut buffer)?;
+    man.render(&mut File::create(out_dir.join("asciinema.1"))?)?;
 
-    fs::write(out_dir.join("asciinema.1"), buffer)
+    Ok(())
 }
