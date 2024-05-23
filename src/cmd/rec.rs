965be1117f0a856f1026e765476600394dc14862
Marcin Kulik <m@ku1ik.com>
2024-05-23 21:38:23 +0200

    Collapse nested ifs

--- src/cmd/rec.rs
+++ src/cmd/rec.rs
@@ -116,13 +116,11 @@ impl cli::Record {
     fn get_tty(&self) -> Result<Box<dyn tty::Tty>> {
         if self.headless {
             Ok(Box::new(tty::NullTty::open()?))
+        } else if let Ok(dev_tty) = tty::DevTty::open() {
+            Ok(Box::new(dev_tty))
         } else {
-            if let Ok(dev_tty) = tty::DevTty::open() {
-                Ok(Box::new(dev_tty))
-            } else {
-                logger::info!("TTY not available, recording in headless mode");
-                Ok(Box::new(tty::NullTty::open()?))
-            }
+            logger::info!("TTY not available, recording in headless mode");
+            Ok(Box::new(tty::NullTty::open()?))
         }
     }
 
