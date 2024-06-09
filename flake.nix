48c89039a6e0ef19a78b7a25a4235c0961fe3f9f
b4mbus <github@yonderly.addy.io>
2024-06-09 02:42:39 +0200

    ref(nix): remove unnecessary explicit inputs

--- flake.nix
+++ flake.nix
@@ -15,9 +15,7 @@
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
       perSystem = {
-        config,
         self',
-        inputs',
         pkgs,
         system,
         ...
