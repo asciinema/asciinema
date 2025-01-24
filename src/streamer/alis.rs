bd157ca8749c34a8afe0160c2466160d7feb9a96
Marcin Kulik <m@ku1ik.com>
2025-01-24 11:36:59 +0100

    Use delta coding for timestamps in ALiS

--- src/streamer/alis.rs
+++ src/streamer/alis.rs
@@ -6,7 +6,7 @@
 
 use super::session;
 use anyhow::Result;
-use futures_util::{stream, Stream, StreamExt, TryStreamExt};
+use futures_util::{stream, Stream, StreamExt};
 use std::future;
 use tokio::sync::mpsc;
 use tokio_stream::wrappers::errors::BroadcastStreamRecvError;
@@ -17,17 +17,27 @@ pub async fn stream(
     clients_tx: &mpsc::Sender<session::Client>,
 ) -> Result<impl Stream<Item = Result<Vec<u8>, BroadcastStreamRecvError>>> {
     let header = stream::once(future::ready(Ok(MAGIC_STRING.into())));
-    let events = session::stream(clients_tx).await?.map_ok(encode_event);
+
+    let events = session::stream(clients_tx)
+        .await?
+        .scan(0u64, |prev_event_time, event| {
+            future::ready(Some(event.map(|event| {
+                let (bytes, time) = encode_event(event, *prev_event_time);
+                *prev_event_time = time;
+
+                bytes
+            })))
+        });
 
     Ok(header.chain(events))
 }
 
-fn encode_event(event: session::Event) -> Vec<u8> {
+fn encode_event(event: session::Event, prev_event_time: u64) -> (Vec<u8>, u64) {
     use session::Event::*;
 
     match event {
         Init(time, size, theme, init) => {
-            let time_bytes = time.to_le_bytes();
+            let time_bytes = encode_rel_time(time);
             let (cols, rows): (u16, u16) = (size.0, size.1);
             let cols_bytes = cols.to_le_bytes();
             let rows_bytes = rows.to_le_bytes();
@@ -35,7 +45,7 @@ fn encode_event(event: session::Event) -> Vec<u8> {
             let init_len_bytes = init_len.to_le_bytes();
 
             let mut msg = vec![0x01]; // 1 byte
-            msg.extend_from_slice(&time_bytes); // 8 bytes
+            msg.extend_from_slice(&time_bytes); // 2-9 bytes
             msg.extend_from_slice(&cols_bytes); // 2 bytes
             msg.extend_from_slice(&rows_bytes); // 2 bytes
 
@@ -64,60 +74,80 @@ fn encode_event(event: session::Event) -> Vec<u8> {
             msg.extend_from_slice(&init_len_bytes); // 4 bytes
             msg.extend_from_slice(init.as_bytes()); // init_len bytes
 
-            msg
+            (msg, time)
         }
 
         Output(time, text) => {
-            let time_bytes = time.to_le_bytes();
+            let time_bytes = encode_rel_time(time - prev_event_time);
             let text_len = text.len() as u32;
             let text_len_bytes = text_len.to_le_bytes();
 
             let mut msg = vec![b'o']; // 1 byte
-            msg.extend_from_slice(&time_bytes); // 8 bytes
+            msg.extend_from_slice(&time_bytes); // 2-9 bytes
             msg.extend_from_slice(&text_len_bytes); // 4 bytes
             msg.extend_from_slice(text.as_bytes()); // text_len bytes
 
-            msg
+            (msg, time)
         }
 
         Input(time, text) => {
-            let time_bytes = time.to_le_bytes();
+            let time_bytes = encode_rel_time(time - prev_event_time);
             let text_len = text.len() as u32;
             let text_len_bytes = text_len.to_le_bytes();
 
             let mut msg = vec![b'i']; // 1 byte
-            msg.extend_from_slice(&time_bytes); // 8 bytes
+            msg.extend_from_slice(&time_bytes); // 2-9 bytes
             msg.extend_from_slice(&text_len_bytes); // 4 bytes
             msg.extend_from_slice(text.as_bytes()); // text_len bytes
 
-            msg
+            (msg, time)
         }
 
         Resize(time, size) => {
-            let time_bytes = time.to_le_bytes();
+            let time_bytes = encode_rel_time(time - prev_event_time);
             let (cols, rows): (u16, u16) = (size.0, size.1);
             let cols_bytes = cols.to_le_bytes();
             let rows_bytes = rows.to_le_bytes();
 
             let mut msg = vec![b'r']; // 1 byte
-            msg.extend_from_slice(&time_bytes); // 8 bytes
+            msg.extend_from_slice(&time_bytes); // 2-9 bytes
             msg.extend_from_slice(&cols_bytes); // 2 bytes
             msg.extend_from_slice(&rows_bytes); // 2 bytes
 
-            msg
+            (msg, time)
         }
 
         Marker(time, text) => {
-            let time_bytes = time.to_le_bytes();
+            let time_bytes = encode_rel_time(time - prev_event_time);
             let text_len = text.len() as u32;
             let text_len_bytes = text_len.to_le_bytes();
 
             let mut msg = vec![b'm']; // 1 byte
-            msg.extend_from_slice(&time_bytes); // 8 bytes
+            msg.extend_from_slice(&time_bytes); // 2-9 bytes
             msg.extend_from_slice(&text_len_bytes); // 4 bytes
             msg.extend_from_slice(text.as_bytes()); // text_len bytes
 
-            msg
+            (msg, time)
         }
     }
+}
+
+fn encode_rel_time(rel_time: u64) -> Vec<u8> {
+    let mut msg = vec![];
+
+    if rel_time < 256 {
+        msg.push(1);
+        msg.push(rel_time as u8);
+    } else if rel_time < 65_536 {
+        msg.push(2);
+        msg.extend_from_slice(&(rel_time as u16).to_le_bytes());
+    } else if rel_time < 4_294_967_296 {
+        msg.push(4);
+        msg.extend_from_slice(&(rel_time as u32).to_le_bytes());
+    } else {
+        msg.push(8);
+        msg.extend_from_slice(&rel_time.to_le_bytes());
+    }
+
+    msg
 }
