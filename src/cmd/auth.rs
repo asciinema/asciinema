61a83d22763fa7b3ab99e26ee0b99ecd540f0d71
Marcin Kulik <m@ku1ik.com>
2025-08-29 20:37:08 +0200

    Mention streaming in `asciinema auth` message

--- src/cmd/auth.rs
+++ src/cmd/auth.rs
@@ -11,9 +11,9 @@ impl cli::Auth {
         let server_hostname = server_url.host().unwrap();
         let auth_url = api::get_auth_url(&config)?;
 
-        println!("Open the following URL in a web browser to authenticate this asciinema CLI with your {server_hostname} user account:\n");
+        println!("Open the following URL in a web browser to authenticate this CLI with your {server_hostname} user account:\n");
         println!("{auth_url}\n");
-        println!("This action will associate all recordings uploaded from this machine (past and future ones) with your account, allowing you to manage them (change the title/theme, delete) at {server_hostname}.");
+        println!("This will associate all recordings uploaded from this machine with your account (including past uploads), and enable public live streaming via {server_hostname}.");
 
         Ok(())
     }
